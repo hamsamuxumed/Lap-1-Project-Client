73ddfc93d4abe483ccb0936abfcb9ea708383a63
Max Mackie <maxmackie13@gmail.com>
2021-10-04 18:12:22 +0100

    adding gif search and display functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
+// L8NZ2ooXz2otBXVDkffxMXzUVYL7AuQ3 this is our giphy API key
+
 const search = document.querySelector('#search');
 search.addEventListener('change', async (event) => {
     const searchTerm = event.srcElement.value;
     console.log(searchTerm);
-    const res = await axios.get(`https://api.giphy.com/v1/gifs/trending?api_key=L8NZ2ooXz2otBXVDkffxMXzUVYL7AuQ3&limit=1&q=${searchTerm}`);
-    const data = res.data;
-    console.log(data)
-    let source = res.data.images.original.url
-    // console.log(res.data);
-    // console.log(res.data[0]);
-//     const iframe = document.createElement('IFRAME');
-//     iframe.src = res.data[0].url;
-//     document.body.append(iframe);
+    const res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=L8NZ2ooXz2otBXVDkffxMXzUVYL7AuQ3&limit=3&q=${searchTerm}`);
+    console.log(res.data.data[0].images.original.url)
+    console.log(res.data.data);
+    const iframe1 = document.createElement('IFRAME');
+    const iframe2 = document.createElement('IFRAME');
+    const iframe3 = document.createElement('IFRAME');
+    iframe1.src = res.data.data[0].images.original.url;
+    iframe2.src = res.data.data[1].images.original.url;
+    iframe3.src = res.data.data[2].images.original.url;
+    document.querySelector('.gifSearch').append(iframe1);
+    document.querySelector('.gifSearch').append(iframe2);
+    document.querySelector('.gifSearch').append(iframe3);
 });
 
-// L8NZ2ooXz2otBXVDkffxMXzUVYL7AuQ3 this is our giphy API key
