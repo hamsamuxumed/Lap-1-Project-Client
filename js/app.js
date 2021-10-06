cfa2c17becd5ee6fe987d79675baa41f792adc11
Max Mackie <maxmackie13@gmail.com>
2021-10-06 09:38:53 +0100

    improving usability of newArticle function

--- js/app.js
+++ js/app.js
@@ -37,93 +37,98 @@ window.addEventListener('load', async (e) => {
     console.log(dataObject)
     const main = document.querySelector('main')
     for (let entry of dataObject){
-        // create header
-        const newHeader = document.createElement('header')
-        const newUsername = document.createElement('p')
-        const newDate = document.createElement('p')
-        newHeader.classList.add('entryHeader')
-        newUsername.classList.add('entryUsername')
-        newDate.classList.add('entryDate')
-        newUsername.href = 'goes to page of just entries from that author'
-        newUsername.append(entry.username)
-        newDate.append(entry.date)
-        newHeader.append(newUsername)
-        newHeader.append(newDate)
-        //create title and text
-        const newTitle = document.createElement('h2')
-        const newMessage = document.createElement('p')
-        newTitle.append(entry.title)
-        newMessage.append(entry.message)
-        //create gif section
-        const newSection = document.createElement('section')
-        const newIframe = document.createElement('iframe')
-        newSection.classList.add('gif')
-        newIframe.classList.add('giphy-embed')
-        newIframe.src = entry.gifUrl
-        newIframe.width = "480"
-        newIframe.height = "331"
-        newIframe.border = "0"
-        newSection.append(newIframe)
-        //create footer
-        const newFooter = document.createElement('footer')
-        const newCommentsDiv = document.createElement('div')
-        const newButton = document.createElement('button')
-        const newEmojiDiv = document.createElement('div')
-        const newHappyP = document.createElement('p')
-        const newLoveP = document.createElement('p')
-        const newAngryP = document.createElement('p')
-        const newHappySpan = document.createElement('span')
-        const newLoveSpan = document.createElement('span')
-        const newAngrySpan = document.createElement('span')
-        newFooter.classList.add('entryFooter')
-        newCommentsDiv.classList.add('comments')
-        newButton.classList.add('commentSectionButton')
-        newEmojiDiv.classList.add('entryEmojiSection')
-        newHappyP.classList.add('happy')
-        newLoveP.classList.add('love')
-        newAngryP.classList.add('angry')
-        newButton.append('comments section')
-        newCommentsDiv.append(newButton)
-        newHappySpan.append('&#128512;') 
-        newHappyP.innerHTML = `<span>&#128512;</span> ${entry.reactions.happy}`
-        newLoveP.innerHTML = `<span>&#10084;&#65039;</span> ${entry.reactions.love}`
-        newAngryP.innerHTML = `<span>&#128545;</span> ${entry.reactions.angry}`
-        newEmojiDiv.append(newHappyP)
-        newEmojiDiv.append(newLoveP)
-        newEmojiDiv.append(newAngryP)
-        newFooter.append(newCommentsDiv)
-        newFooter.append(newEmojiDiv)
-
-        //create article
-        const newArticle = document.createElement('article')
-        newArticle.classList.add('entryCard')
-        newArticle.append(newHeader)
-        newArticle.append(newTitle)
-        newArticle.append(newMessage)
-        newArticle.append(newSection)
-        newArticle.append(newFooter)
-        
-        const newCommentsSection = document.createElement('section');
-        newArticle.append(newCommentsSection)
-
-        //create comments section
-        for (let comment of entry.comments) {
-            const newCommentsArticle = document.createElement('article');
-            const newAuthor = document.createElement('h3');
-            const newText = document.createElement('p');
-            newCommentsSection.classList.add('commentsSection');
-            newCommentsSection.classList.add('noDisplay');
-            newCommentsArticle.classList.add('comment');
-            newAuthor.classList.add('commentAuthor');
-            newText.classList.add('commentText');
-            newText.append(comment.text);
-            newAuthor.append(comment.author); 
-            newCommentsArticle.append(newAuthor);
-            newCommentsArticle.append(newText);
-            newCommentsSection.append(newCommentsArticle);
-        }
-
+        const newArticle = createNewEntry(entry)
         main.append(newArticle)
     }
 })
+
+
+const createNewEntry = (entry) => {
+    // create header
+    const newHeader = document.createElement('header')
+    const newUsername = document.createElement('p')
+    const newDate = document.createElement('p')
+    newHeader.classList.add('entryHeader')
+    newUsername.classList.add('entryUsername')
+    newDate.classList.add('entryDate')
+    newUsername.href = 'goes to page of just entries from that author'
+    newUsername.append(entry.username)
+    newDate.append(entry.date)
+    newHeader.append(newUsername)
+    newHeader.append(newDate)
+    //create title and text
+    const newTitle = document.createElement('h2')
+    const newMessage = document.createElement('p')
+    newTitle.append(entry.title)
+    newMessage.append(entry.message)
+    //create gif section
+    const newSection = document.createElement('section')
+    const newIframe = document.createElement('iframe')
+    newSection.classList.add('gif')
+    newIframe.classList.add('giphy-embed')
+    newIframe.src = entry.gifUrl
+    newIframe.width = "480"
+    newIframe.height = "331"
+    newIframe.border = "0"
+    newSection.append(newIframe)
+    //create footer
+    const newFooter = document.createElement('footer')
+    const newCommentsDiv = document.createElement('div')
+    const newButton = document.createElement('button')
+    const newEmojiDiv = document.createElement('div')
+    const newHappyP = document.createElement('p')
+    const newLoveP = document.createElement('p')
+    const newAngryP = document.createElement('p')
+    const newHappySpan = document.createElement('span')
+    const newLoveSpan = document.createElement('span')
+    const newAngrySpan = document.createElement('span')
+    newFooter.classList.add('entryFooter')
+    newCommentsDiv.classList.add('comments')
+    newButton.classList.add('commentSectionButton')
+    newEmojiDiv.classList.add('entryEmojiSection')
+    newHappyP.classList.add('happy')
+    newLoveP.classList.add('love')
+    newAngryP.classList.add('angry')
+    newButton.append('comments section')
+    newCommentsDiv.append(newButton)
+    newHappySpan.append('&#128512;') 
+    newHappyP.innerHTML = `<span>&#128512;</span> ${entry.reactions.happy}`
+    newLoveP.innerHTML = `<span>&#10084;&#65039;</span> ${entry.reactions.love}`
+    newAngryP.innerHTML = `<span>&#128545;</span> ${entry.reactions.angry}`
+    newEmojiDiv.append(newHappyP)
+    newEmojiDiv.append(newLoveP)
+    newEmojiDiv.append(newAngryP)
+    newFooter.append(newCommentsDiv)
+    newFooter.append(newEmojiDiv)
+
+    //create article
+    const newArticle = document.createElement('article')
+    newArticle.classList.add('entryCard')
+    newArticle.append(newHeader)
+    newArticle.append(newTitle)
+    newArticle.append(newMessage)
+    newArticle.append(newSection)
+    newArticle.append(newFooter)
+
+    const newCommentsSection = document.createElement('section');
+    newArticle.append(newCommentsSection)
+
+    //create comments section
+    for (let comment of entry.comments) {
+        const newCommentsArticle = document.createElement('article');
+        const newAuthor = document.createElement('h3');
+        const newText = document.createElement('p');
+        newCommentsSection.classList.add('commentsSection');
+        newCommentsSection.classList.add('noDisplay');
+        newCommentsArticle.classList.add('comment');
+        newAuthor.classList.add('commentAuthor');
+        newText.classList.add('commentText');
+        newText.append(comment.text);
+        newAuthor.append(comment.author); 
+        newCommentsArticle.append(newAuthor);
+        newCommentsArticle.append(newText);
+        newCommentsSection.append(newCommentsArticle);
+    }
+    return newArticle
+}
 
