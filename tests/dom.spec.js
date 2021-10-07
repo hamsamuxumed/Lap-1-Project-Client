eb53656b79a6a24197151e2d31c3298dba981cfb
Forum-123 <69541616+Forum-123@users.noreply.github.com>
2021-10-07 15:54:41 +0100

    Add testing to layout

--- tests/dom.spec.js
+++ tests/dom.spec.js
@@ -1,3 +1,3 @@
-const fs = require("fs");
-const path = require("path");
-const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf-8");
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf-8');
